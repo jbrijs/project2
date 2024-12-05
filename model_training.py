5b76d585a98935ee6ce7a5165cfaf8fe1fd711e3
jbrijs <joebrijs@gmail.com>
2024-12-04 17:51:12 -0700

    training loop start

--- model_training.py
+++ model_training.py
@@ -1,6 +1,7 @@
 import torch 
 import torch.nn as nn
 import torch.optim as optim
+import pandas as pd
 
 class LSTMModel(nn.Module):
     def __init__(self, input_size, hidden_size, num_layers, output_size):
@@ -16,3 +17,35 @@ class LSTMModel(nn.Module):
         out, _ = self.lstm(x, (h0, c0))
         out = self.fc(out[:, -1, :])
         return out
+    
+'''
+BASELINE MODEL
+
+Model trained on just Open, High, Low, Volume, and finding Close
+'''
+
+def train_test_split_simple(ticker):
+    df = pd.read_csv(f"./data/{ticker}_split_daily_data.csv")
+
+
+    # Reverse df so it is in order
+    df = df.iloc[::-1].reset_index(drop=True)
+
+    X = df.drop(['time_stamp', 'close'], axis=1)
+
+    y = df['close']
+
+    X_tensor = torch.tensor(X.to_numpy())
+    y_tensor = torch.tensor(y.to_numpy())
+
+    return X_tensor, y_tensor
+    
+
+if __name__ == '__main__':
+    train_test_split_simple('AAPL')
+
+def train_simple_model(X, y):
+    model = LSTMModel(input_size=X.size, hidden_size=50, num_layers=50, output_size=1)
+
+    criterion = nn.MSELoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
